4ef4e0f523f42ea4c2b70bf0c44f4909241ea0c7
dkpark10 <dkpark10@naver.com>
2025-08-12 15:07:53 +0900

    feat: 게시글 캐시

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,12 +3,6 @@ import Link from 'next/link';
 
 export default function Home() {
   const allPosts = getPosts();
-  // {
-  //   slug: '타입 세이프하게 api를 호출해보자',
-  //   title: '타입 세이프하게 api를 호출해보기',
-  //   date: '2024-05-04',
-  //   description: '오버 엔지니어링인 감이 없지 않아 있다.'
-  // }
 
   return (
     <ul>
--- src/utils/get-posts.js
+++ src/utils/get-posts.js
@@ -1,31 +1,42 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import { generateTitleSlug } from '@/utils/generate-title-slug';
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { generateTitleSlug } from "@/utils/generate-title-slug";
 
-const postsDirectory = path.join(process.cwd(), '/src/content');
+const postsDirectory = path.join(process.cwd(), "/src/content");
 
+/** @type {Array<{ slug: string, orgFileName: string }> | null} */
+let cache = null;
+
+/**
+ * 사용자 객체를 생성합니다.
+ * @returns {Array<{ slug: string, orgFileName: string }>}
+ */
 export const getPosts = () => {
+  if (cache) return cache;
   const files = fs.readdirSync(postsDirectory);
 
-  return files
-    .filter(file => file.endsWith('.mdx'))
-    .map(fileName => {
+  const results = files
+    .filter((file) => file.endsWith(".mdx"))
+    .map((fileName) => {
       const filePath = path.join(postsDirectory, fileName);
 
-      const fileContents = fs.readFileSync(filePath, 'utf-8');
+      const fileContents = fs.readFileSync(filePath, "utf-8");
 
       const { data } = matter(fileContents);
 
       return {
-        slug: generateTitleSlug(fileName.replace(/\.mdx$/, '')),
+        slug: generateTitleSlug(fileName.replace(/\.mdx$/, "")),
         orgFileName: fileName,
         ...data,
       };
     })
     .sort((a, b) => new Date(b.date) - new Date(a.date)); // 최신순
-}
+  cache = results;
+  return results;
+};
 
 export const getOrgFileName = (slug) => {
+  if (cache) return cache;
   return getPosts().find((post) => post.slug === slug).orgFileName;
-}
+};
