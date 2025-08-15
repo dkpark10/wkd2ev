7fe28398095070646fa86330502638645e114250
dkpark10 <dkpark10@naver.com>
2025-08-15 16:36:48 +0900

    feat: code theme 적용

--- mdx-components.jsx
+++ mdx-components.jsx
@@ -1,6 +1,15 @@
 import '@/styles/mdx-components.scss';
+import { PrismLight } from 'react-syntax-highlighter';
+import nightOwl from 'react-syntax-highlighter/dist/esm/styles/prism/night-owl';
 
-const components = {};
+const components = {
+  code: ({ children }) => (
+    <PrismLight language="javascript" style={nightOwl}>
+      {children}
+    </PrismLight>
+  ),
+
+};
 
 export function useMDXComponents() {
   return components
