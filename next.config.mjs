95b6a024e2fa09720d7df89526a59e9e6366c888
dkpark10 <dkpark10@naver.com>
2025-08-20 15:31:53 +0900

    chore: 정적 빌드

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,14 @@ const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
+
+  images: {
+    unoptimized: true,
+  },
+
+  output: "export",
+
+  reactStrictMode: true,
 };
 
 const withMDX = createMDX({
