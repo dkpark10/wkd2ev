358df8cfacadf2a0f3193f19fae4530cb6e000d9
dkpark10 <dkpark10@naver.com>
2025-10-21 23:45:17 +0900

    feat: 다크모드 구현

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,8 @@
 import "@/styles/globals.css";
 import "@/styles/_base.scss";
 import Link from "next/link";
-import Github from "@/components/github";
 import { SubTitleProvider } from "@/components/sub-title-context";
+import DarkModeButton from "@/components/dark-mode.button";
 
 export const metadata = {
   title: "wkd2ev 블로그",
@@ -13,10 +13,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <body>
-        
+
         <header>
           <Link className="title" href="/">wkd2ev</Link>
-          <Link href="https://github.com/dkpark10"><Github /></Link>
+
+          <div className="right">
+
+            <DarkModeButton />
+
+            <Link href="https://github.com/dkpark10" className="github-container">
+              <img src='/icons/github.svg' width={24} height={24} alt='깃허브 아이콘' />
+            </Link>
+          </div>
         </header>
 
         <div className="container">
---
+++ src/components/dark-mode.button.jsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import clsx from "clsx";
+
+function Sun() {
+  return (
+    <img src='/icons/sun.svg' width={16} height={16} alt='태양 아이콘' />
+  );
+}
+
+function Moon() {
+  return (
+    <img src='/icons/moon.svg' width={16} height={16} alt='달 아이콘' />
+  )
+}
+
+const KEY = 'THEME';
+
+export default function DarkModeButton() {
+  /** @type {['light' | 'dark' | null, React.Dispatch<React.SetStateAction<'light' | 'dark'>>]} */
+  const [theme, setTheme] = useState('light');
+
+  const [animationTrigger, setAnimationTrigger] = useState(false);
+
+  useEffect(() => {
+    const currentTheme = localStorage.getItem(KEY) || 'light';
+    setTheme(currentTheme);
+    document.body.dataset.theme = currentTheme;
+  }, []);
+
+  const onClick = () => {
+    const next = theme === "light" ? "dark" : "light";
+    localStorage.setItem(KEY, next);
+    document.body.dataset.theme = next;
+    setTheme(next);
+    setAnimationTrigger((prev) => !prev);
+  }
+
+  return (
+    <div
+      className="dark-container"
+      onClick={onClick}
+      role='button'
+    >
+      <button
+        className={clsx(animationTrigger && 'active')}
+        type='button'
+        role='switch'
+      >
+        {theme === 'dark' ? <Moon /> : <Sun />}
+      </button>
+    </div>
+  )
+}
