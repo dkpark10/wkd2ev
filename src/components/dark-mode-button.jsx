5abe7b42b91f2ab2689f467b78d35c6012d4f330
dkpark10 <dkpark10@naver.com>
2025-11-09 20:06:20 +0900

    style: 토글 초기 렌더링 조건 수정 다크모드

--- src/components/dark-mode-button.jsx
+++ src/components/dark-mode-button.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import clsx from "clsx";
 import Image from "@/components/image";
 
@@ -20,9 +20,9 @@ const KEY = 'THEME';
 
 export default function DarkModeButton() {
   /** @type {['light' | 'dark' | null, React.Dispatch<React.SetStateAction<'light' | 'dark'>>]} */
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState(null);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const currentTheme = localStorage.getItem(KEY) || 'light';
     setTheme(currentTheme);
   }, []);
@@ -40,13 +40,15 @@ export default function DarkModeButton() {
       onClick={onClick}
       role='button'
     >
-      <button
-        className={clsx(theme === 'dark' && 'active')}
-        type='button'
-        role='switch'
-      >
-        {theme === 'dark' ? <Moon /> : <Sun />}
-      </button>
+      {theme === null ? <></> : (
+        <button
+          className={clsx(theme === 'dark' && 'active', 'fade_in')}
+          type='button'
+          role='switch'
+        >
+          {theme === 'dark' ? <Moon /> : <Sun />}
+        </button>
+      )}
     </div>
   )
 }
